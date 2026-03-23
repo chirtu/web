239dc44b548f41e69b5983149d8e86c48d244a3f
chirtu <chitufatuma45@gmail.com>
2026-03-23 12:14:15 +0300

    some changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 (function ($) {
     "use strict";
 
+    // years
+    var yearElement = document.getElementById("year");
+    if (yearElement) {
+        yearElement.textContent = new Date().getFullYear();
+    }
+
 
     // tails
     const dot = document.querySelector(".cursor-dot");
@@ -30,7 +36,7 @@
     if (document.getElementById("particles-js")) {
         particlesJS("particles-js", {
             particles: {
-                number: { value: 80 },
+                number: { value: 100 },
                 color: { value: "#ffffff" },
                 shape: { type: "circle" },
                 opacity: { value: 0.5 },
@@ -51,138 +57,132 @@
                 events: {
                     onhover: {
                         enable: true,
-                        mode: "grab"
+                        mode: "repulse"
                     }
                 },
                 modes: {
-                    grab: {
-                        distance: 200,
-                        line_linked: {
-                            opacity: 1
-                        }
-                    }
+                    move: { enable: true, speed: 3 },
+                    repulse: { distance: 220, duration: 0.6 }
+                    // repulse: {
+                    //     distance: 180,
+                    //     duration: 0.4
+                    // }
+                }
+            },
+            retina_detect: true
+        });
+
+        var spinner = function () {
+            setTimeout(function () {
+                if ($('#spinner').length > 0) {
+                    $('#spinner').removeClass('show');
+                }
+            }, 1);
+        };
+        spinner(0);
+
+
+        // Initiate the wowjs
+        new WOW().init();
+
+
+        // Sticky Navbar
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 45) {
+                $('.navbar').addClass('sticky-top shadow-sm');
+            } else {
+                $('.navbar').removeClass('sticky-top shadow-sm');
+            }
+        });
+
+
+        // testimonial carousel
+        $(".testimonial-carousel").owlCarousel({
+            autoplay: true,
+            smartSpeed: 1500,
+            center: false,
+            dots: true,
+            loop: true,
+            margin: 25,
+            nav: true,
+            navText: [
+                '<i class="fa fa-angle-right"></i>',
+                '<i class="fa fa-angle-left"></i>'
+            ],
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                576: {
+                    items: 1
+                },
+                768: {
+                    items: 1
+                },
+                992: {
+                    items: 2
+                },
+                1200: {
+                    items: 2
                 }
             }
         });
+
+
+        // training carousel
+        $(".training-carousel").owlCarousel({
+            autoplay: true,
+            smartSpeed: 1500,
+            center: false,
+            dots: true,
+            loop: true,
+            margin: 25,
+            nav: true,
+            navText: [
+                '<i class="fa fa-angle-right"></i>',
+                '<i class="fa fa-angle-left"></i>'
+            ],
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                576: {
+                    items: 1
+                },
+                768: {
+                    items: 2
+                },
+                992: {
+                    items: 3
+                },
+                1200: {
+                    items: 4
+                }
+            }
+        });
+
+
+
+
+
+        // Facts counter
+        $('[data-toggle="counter-up"]').counterUp({
+            delay: 5,
+            time: 2000
+        });
+
+
+        // Back to top button
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 300) {
+                $('.back-to-top').fadeIn('slow');
+            } else {
+                $('.back-to-top').fadeOut('slow');
+            }
+        });
     }
-
-
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner(0);
-
-
-    // Initiate the wowjs
-    new WOW().init();
-
-
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 45) {
-            $('.navbar').addClass('sticky-top shadow-sm');
-        } else {
-            $('.navbar').removeClass('sticky-top shadow-sm');
-        }
-    });
-
-
-    // testimonial carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        center: false,
-        dots: true,
-        loop: true,
-        margin: 25,
-        nav: true,
-        navText: [
-            '<i class="fa fa-angle-right"></i>',
-            '<i class="fa fa-angle-left"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            576: {
-                items: 1
-            },
-            768: {
-                items: 1
-            },
-            992: {
-                items: 2
-            },
-            1200: {
-                items: 2
-            }
-        }
-    });
-
-
-    // training carousel
-    $(".training-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        center: false,
-        dots: true,
-        loop: true,
-        margin: 25,
-        nav: true,
-        navText: [
-            '<i class="fa fa-angle-right"></i>',
-            '<i class="fa fa-angle-left"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            576: {
-                items: 1
-            },
-            768: {
-                items: 2
-            },
-            992: {
-                items: 3
-            },
-            1200: {
-                items: 4
-            }
-        }
-    });
-
-
-
-
-
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 5,
-        time: 2000
-    });
-
-
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
-        return false;
-    });
-
 
 })(jQuery);
 
